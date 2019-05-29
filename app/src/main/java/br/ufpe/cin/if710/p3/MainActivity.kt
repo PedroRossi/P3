33c73d5a483c499875c0362a924760def15b6b4a
Pedro <pedro@pop-os.localdomain>
2019-05-28 22:42:29 -0300

    Navigation fragment prototype added

--- app/src/main/java/br/ufpe/cin/if710/p3/MainActivity.kt
+++ app/src/main/java/br/ufpe/cin/if710/p3/MainActivity.kt
@@ -1,36 +1,46 @@
-package com.example.p3
+package br.ufpe.cin.if710.p3
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
+import com.example.p3.R
+import android.support.v4.app.Fragment
+import br.ufpe.cin.if710.p3.fragments.HistoryFragment
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textMessage: TextView
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                textMessage.setText(R.string.title_home)
+            R.id.navigation_history -> {
+                val fragment = HistoryFragment.newInstance()
+                this.openFragment(fragment)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                textMessage.setText(R.string.title_dashboard)
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                textMessage.setText(R.string.title_notifications)
+
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        textMessage = findViewById(R.id.message)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     }
 }
---
+++ app/src/main/java/br/ufpe/cin/if710/p3/fragments/HistoryFragment.kt
@@ -0,0 +1,22 @@
+package br.ufpe.cin.if710.p3.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.p3.R
+
+class HistoryFragment : Fragment() {
+
+    companion object {
+        fun newInstance(): HistoryFragment {
+            return HistoryFragment()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.history, container, false)
+    }
+
+}
